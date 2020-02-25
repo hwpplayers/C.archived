b30480c014b7fca0de36067bb41cf1891cf2019e
hwpplayer1 <mertgor@openmailbox.org>
2020-02-25 23:55:24 +0300

    Fonksiyon tanımlama

---
+++ function.c
@@ -0,0 +1,22 @@
+/*
+Enes Selim
+C'de bir sembolü (değişken, fonksiyon gibi şeylere denir) kullanmadan önce "deklare" etmeniz gerekir.
+Deklare edilmiş sembollerin de "tanımlanması" gerekir yoksa undefined reference hatası alırsınız.
+Mesela void foo(); bir deklerasyondur, derleyiciye böyle bir fonksiyon olduğunu söyler ama bunun nasıl çalıştığını, bu fonksiyonun ne yaptığını anlatmaz.İsterseniz deklerasyonla tanımlamayı birleştirebilirsiniz, main ve foo'da yaptığınız bu Şimdi main'den önce void foo(); yazın
+Ve bu şekilde deneyin
+*/
+
+#include <stdio.h>
+
+void foo()
+{
+  printf("I am foo\n");
+}
+
+int main()
+{
+  foo();
+
+}
+
+
