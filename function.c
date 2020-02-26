6cd20af8d825e0e71f7591700dda8146f80a48c2
hwpplayer1 <mertgor@openmailbox.org>
2020-02-26 19:20:13 +0300

    Fonksiyon tanımlama

--- function.c
+++ function.c
@@ -5,7 +5,10 @@ Deklare edilmiş sembollerin de "tanımlanması" gerekir yoksa undefined referen
 Mesela void foo(); bir deklerasyondur, derleyiciye böyle bir fonksiyon olduğunu söyler ama bunun nasıl çalıştığını, bu fonksiyonun ne yaptığını anlatmaz.İsterseniz deklerasyonla tanımlamayı birleştirebilirsiniz, main ve foo'da yaptığınız bu Şimdi main'den önce void foo(); yazın
 Ve bu şekilde deneyin
 */
-
+/*[geri dönüş değerinin türü] <fonksiyon ismi> ([parametre bildirimi])
+{
+	/* ... */
+}*/
 #include <stdio.h>
 
 void foo()
