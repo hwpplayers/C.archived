288ba962f14e85f53e89b7175c4cc887ec9cf248
hwpplayer1 <mertgor@openmailbox.org>
2020-02-26 21:10:54 +0300

    Double

---
+++ double.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+  double a,b,c;
+  printf("a degerini giriniz :");
+  scanf("%lf" , &a);
+  printf("b degerini giriniz :");
+  scanf("%lf", &b);
+  c = a + b;
+  printf("c = %f\n",c);
+}
