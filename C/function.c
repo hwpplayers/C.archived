649e7aee479ae844157f9069c4a45fd314cafeba
Mert Gör <mertgor@masscollabs.com>
2020-05-28 06:52:41 +0300

    NXiss7 original author
    NXiss7 original author

--- C/function.c
+++ C/function.c
@@ -1,5 +1,5 @@
 /*
-Enes Selim
+NXiss7
 C'de bir sembolü (değişken, fonksiyon gibi şeylere denir) kullanmadan önce "deklare" etmeniz gerekir.
 Deklare edilmiş sembollerin de "tanımlanması" gerekir yoksa undefined reference hatası alırsınız.
 Mesela void foo(); bir deklerasyondur, derleyiciye böyle bir fonksiyon olduğunu söyler ama bunun nasıl çalıştığını, bu fonksiyonun ne yaptığını anlatmaz.İsterseniz deklerasyonla tanımlamayı birleştirebilirsiniz, main ve foo'da yaptığınız bu Şimdi main'den önce void foo(); yazın
